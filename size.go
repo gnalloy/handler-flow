31fb5f1b92d3f29ca8e3e177d37b1f3486f32eb0
xigexb <xigexb@aliyun.com>
2026-09-01 00:02:13 +0800

    chore: bootstrap handler-flow

---
+++ size.go
@@ -0,0 +1,35 @@
+package flow
+
+import (
+	"gnalloy.org/gnalloy/buffer"
+	"gnalloy.org/gnalloy/message"
+)
+
+// MessageSize 返回入站流控队列使用的消息字节数。
+func MessageSize(msg any) int {
+	switch v := msg.(type) {
+	case nil:
+		return 0
+	case buffer.ByteBuf:
+		return v.ReadableBytes()
+	case []byte:
+		return len(v)
+	case string:
+		return len(v)
+	case interface{ ReadableBytes() int }:
+		return v.ReadableBytes()
+	default:
+		return 0
+	}
+}
+
+func releaseMessage(msg any) {
+	message.Release(msg)
+}
+
+func nonNegative(value int) int {
+	if value < 0 {
+		return 0
+	}
+	return value
+}
